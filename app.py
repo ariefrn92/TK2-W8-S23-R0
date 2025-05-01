07def8e1cacaf0be347c868fd30deb57bd5d11d6
Arief <ariefrn@jabarprov.go.id>
2025-05-01 08:34:48 +0700

    initial init

---
+++ app.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+
+# Muat Model CNN yang sudah tersimpan
+model = tf.keras.models.load_model("model.h5")
+
+# Daftar Nama Breed Kucing
+cat_breed_names = [
+    "Abyssinian", "Bengal", "Birman", "Bombay",
+    "British Shorthair", "Egyptian Mau", "Maine Coon",
+    "Persian", "Ragdoll", "Russian Blue", "Siamese", "Sphynx"
+]
+
+IMG_SIZE = 160
+
+def preprocess_image(image):
+    image = image.resize((IMG_SIZE, IMG_SIZE))
+    image = np.array(image) / 255.0  # Normalisasi
+    image = np.expand_dims(image, axis=0)  # Tambahkan batch dimension
+    return image
+
+# UI Streamlit
+st.title("Klasifikasi Breed Kucing")
+st.write("Unggah gambar kucing untuk mendapatkan prediksi breed-nya.")
+
+uploaded_file = st.file_uploader("Unggah Gambar", type=["jpg", "png", "jpeg"])
+
+if uploaded_file:
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Gambar yang diunggah", use_column_width=True)
+
+    processed_image = preprocess_image(image)
+    prediction = model.predict(processed_image)
+    predicted_label = np.argmax(prediction)
+    predicted_breed = cat_breed_names[predicted_label]
+
+    st.write(f"Prediksi: **{predicted_breed}**")
